403e16a2d69173223b5484f37bdff355bb6b4ca6
Edwin Yu <edwin.hawk.yu@gmail.com>
2022-09-17 19:11:56 -0400

    Completed part 3 of collector lab. Added Class Views

--- dog_collector/dogs/views.py
+++ dog_collector/dogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Dog
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
 
 # Create your views here.
 
@@ -17,3 +18,15 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog':dog})
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = ['name', 'breed', 'description', 'weight']
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description','weight']
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
